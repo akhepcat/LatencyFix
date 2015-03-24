196f675aec6496987d920ed4f5990f4f4a73c302
Leif Sawyer <leif@denali.net>
2015-03-24 10:29:35 -0800

    cleaner output, based on only what needs to be changed

--- LatencyFix.sh
+++ LatencyFix.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
+DEFIF=$(awk 'BEGIN { IGNORECASE=1 } /^[a-z0-9]+[ \t]+00000000/ { print $1 }' /proc/net/route)
+
+# Grab the current information
+congestctls=$(sysctl -ne net.ipv4.tcp_available_congestion_control)
+def_sys_rmem=$(sysctl -en net.core.rmem_default)
+def_proc_rmem=$(cat /proc/sys/net/core/rmem_default)
+def_sys_wmem=$(sysctl -en net.core.wmem_default)
+def_proc_wmem=$(cat /proc/sys/net/core/wmem_default)
+max_sys_rmem=$(sysctl -en net.core.rmem_max)
+max_proc_rmem=$(cat /proc/sys/net/core/rmem_max)
+max_sys_wmem=$(sysctl -en net.core.wmem_max)
+max_proc_wmem=$(cat /proc/sys/net/core/wmem_max)
+pmtu_disc=$(sysctl -ne net.ipv4.ip_no_pmtu_disc)
+tcp_ecn=$(sysctl -ne net.ipv4.tcp_ecn )
+tcp_fack=$(sysctl -ne net.ipv4.tcp_fack )
+tcp_rfc1337=$(sysctl -ne net.ipv4.tcp_rfc1337 )
+tcp_sack=$(sysctl -ne net.ipv4.tcp_sack )
+tcp_window_scaling=$(sysctl -ne net.ipv4.tcp_window_scaling )
+tcp_timestamps=$(sysctl -ne net.ipv4.tcp_timestamps)
+
+[[ $def_sys_rmem != $def_proc_rmem ]] && echo "rmem_default:  sys($def_sys_rmem) vs proc($def_proc_rmem)"
+[[ $def_sys_wmem != $def_proc_wmem ]] && echo "wmem_default:  sys($def_sys_wmem) vs proc($def_proc_wmem)"
+[[ $max_sys_rmem != $max_proc_rmem ]] && echo "    rmem_max:  sys($max_sys_rmem) vs proc($max_proc_rmem)"
+[[ $max_sys_wmem != $max_proc_wmem ]] && echo "    wmem_max:  sys($max_sys_wmem) vs proc($max_proc_wmem)"
+
 # TCP Window Size to test (MSS) -  IPv6 tunnels are 1266?
 WINDOW=1200
 
-PING=$(ping -c 10 -s ${WINDOW} plugbase.gci.net | grep ^rtt)
+PING=$(ping -c 10 -s ${WINDOW} www.google.com | grep ^rtt)
 #+ PING='rtt min/avg/max/mdev = 11.001/15.618/20.264/3.189 ms'
 RTT=${PING##*= }
 #+ RTT='11.001/15.618/20.264/3.189 ms'
 DELAY=${RTT%%.*}
 #+ DELAY='11'   -- in milliseconds...
 
-echo ${DELAY}
+# echo ${DELAY}
 
 ## bad assumption of 100mb/s of maximum throughput
 
@@ -19,42 +44,68 @@ MILLISECONDS=1000
 
 WINDOW_BYTES=$(( 2* $BW * $DELAY / $MILLISECONDS / 8 ))  # 2x because end-to-end
 
-echo "TCP Window MAX should be ${WINDOW_BYTES}, minimum"
+[[ $max_sys_rmem -ge ${WINDOW_BYTES} ]] && echo "TCP Window max_rmrm should be ${WINDOW_BYTES}, minimum"
+[[ $max_sys_wmem -ge ${WINDOW_BYTES} ]] && echo "TCP Window max_wmrm should be ${WINDOW_BYTES}, minimum"
 
-echo "rmem_default: $(cat /proc/sys/net/core/rmem_default)"
-echo "wmwm_default: $(cat /proc/sys/net/core/wmem_default)"
-echo "    rmem_max: $(cat /proc/sys/net/core/rmem_max)"
-echo "    wmem_max: $(cat /proc/sys/net/core/wmem_max)"
+[[ $pmtu_disc -ne 0 ]] && echo "sysctl -w net.ipv4.ip_no_pmtu_disc=0"
+[[ $tcp_ecn  -ne 0 ]] && echo "sysctl -w net.ipv4.tcp_ecn=0"
+[[ $tcp_fack  -ne 1 ]] && echo "sysctl -w net.ipv4.tcp_fack=1"
+[[ $tcp_rfc1337  -ne 1 ]] && echo "sysctl -w net.ipv4.tcp_rfc1337=1"
+[[ $tcp_sack  -ne 1 ]] && echo "sysctl -w net.ipv4.tcp_sack=1"
+[[ $tcp_window_scaling  -ne 1 ]] && echo "sysctl -w net.ipv4.tcp_window_scaling=1"
 
+
+[[ $DELAY -lt 150 ]] && \
+	[[ $tcp_timestamps -ne 1 ]] && \
+		echo "sysctl -w net.ipv4.tcp_timestamps=1"
+
+[[ $DELAY -gt 300 ]] && \
+	[[ -z "${congestctls##*hybla*}" ]] && \
+		echo "sysctl -w net.ipv4.tcp_congestion_control=hybla"
+
+
+
+if [ 0 -eq 1 ]
+then
 
 echo "---- some other values"
 
 # Some sysctl values for squeezing out performance
 cat <<EOF
-sysctl -w net.ipv4.ip_no_pmtu_disc=0
-sysctl -w net.ipv4.tcp_ecn=0
-sysctl -w net.ipv4.tcp_fack=1
-sysctl -w net.ipv4.tcp_rfc1337=1
-sysctl -w net.ipv4.tcp_sack=1
-sysctl -w net.ipv4.tcp_window_scaling=1
-sysctl -w net.ipv4.tcp_congestion_control=hybla
-
-# Typically enabled for fast networks, adds 10bytes overhead, but provides better congestion control info
-net.ipv4.tcp_timestamps = 0
 
 # this should be uncommented only if it's not working well
 net.ipv4.route.flush = 1
 
+# Increase Linux autotuning TCP buffer limits
+# Set max to 16MB for 1GE and 32M (33554432) or 54M (56623104) for 10GE
+# Don't set tcp_mem itself! Let the kernel scale it based on RAM.
 net.core.rmem_max = 33554432 
 net.core.wmem_max = 33554432
 net.core.rmem_default = 65536
 net.core.wmem_default = 65536
+net.core.optmem_max = 40960
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 65536 16777216
 
-net.ipv4.tcp_rmem = 4096 87380 16777216 
-net.ipv4.tcp_wmem = 4096 87380 16777216 
+# Make room for more TIME_WAIT sockets due to more clients,
+# and allow them to be reused if we run out of sockets
+# Also increase the max packet backlog
+net.core.netdev_max_backlog = 50000
+net.ipv4.tcp_max_syn_backlog = 30000
+net.ipv4.tcp_max_tw_buckets = 2000000
+
+# If your servers talk UDP, also up these limits
+net.ipv4.udp_rmem_min = 8192
+net.ipv4.udp_wmem_min = 8192
+
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_max_syn_backlog = 4096
+net.core.netdev_max_backlog = 2500
 EOF
 
-if [ 0 ];
+if [ 0 -eq 1 ];
 then
   cat <<EOF
 # don't cache ssthresh from previous connection
@@ -72,5 +123,7 @@ echo 6553500 6653500 6753500 > /proc/sys/net/ipv4/tcp_mem
 
 
 EOF
+
+fi
 
 fi
